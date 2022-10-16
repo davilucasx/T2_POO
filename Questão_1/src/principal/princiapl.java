c6bab5de255ee46fd11042b4c7bf19d195e372cc
Lucas <lucasdavi.barros22@gmail.com>
2022-10-16 14:56:34 -0300

    POO

---
+++ Questão_1/src/principal/princiapl.java
@@ -0,0 +1,19 @@
+package principal;
+import java.util.Scanner;
+public class princiapl {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.println("------- Ímpar ou par -------");
+		System.out.println("\nDigite um número: ");
+		int num = input.nextInt();
+		if (num % 2 == 0) {
+			System.out.println("O número " + num + " é par");	
+		}
+		else {
+			System.out.println("O " + num + " é ímpar");
+		}
+		System.out.println("----------------------------");
+	}
+
+}
